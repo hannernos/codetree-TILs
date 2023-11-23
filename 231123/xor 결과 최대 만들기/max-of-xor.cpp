7ee78460401cb2fa12277e28280a7ed290225a7b
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2023-11-23 21:08:31 +0900

    Title: xor 결과 최대 만들기, Time: 1014ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 231123/xor 결과 최대 만들기/max-of-xor.cpp
+++ 231123/xor 결과 최대 만들기/max-of-xor.cpp
@@ -1,71 +1,54 @@
 #include <iostream>
 #include <vector>
-#include<unordered_map>
 using namespace std;
 
 //var
-int n;
-int m;
-int maxRes;
+int n, m;
+int maxRes = 0;
 
-vector<int> mv; // 정수 리스트 받기 
-vector<bool>visited;
+vector<int> mv; // 정수 리스트
+vector<bool> visited;
 
 vector<int> temres; // 현재 결과값
-unordered_map<int, int> mm;
 //func
 
 void run(int depth) {
-    //m개의 숫자를 뽑아 
-    //모두 xor한 결과의 최댓값을 출력하는 코드를 작성해보세요.
-    //이진법으로 변환하고 하라는 건가 ?
     if (depth == m) {
-        // 길이 만족, 종료
-        //최댓값 비교하고 갱신
-        int tem_sum=0;
-        //본문에서만 m이고 for문으로 ㄱㄱ
-        for (int i = 0; i < m-1;i++) {
-            tem_sum = temres[i] ^ temres[i + 1];
+        int tem_sum = 0;
+        for (int i = 0; i < m; i++) {
+            tem_sum ^= temres[i]; 
         }
 
-        if (tem_sum > maxRes) {
-            maxRes = tem_sum;
-        }
+        maxRes = max(maxRes, tem_sum);
+        return;
     }
 
     for (int i = 0; i < n; i++) {
-
         if (visited[i]) {
             continue;
         }
 
         visited[i] = true;
-        temres.push_back(mm[i]);
+        temres.push_back(mv[i]);
         run(depth + 1);
         temres.pop_back();
         visited[i] = false;
     }
-
 }
-void init() {
-    int tem;
-    maxRes = 0;
-    cin >> n >> m;
 
-    visited.push_back(false);
+void init() {
+    cin >> n >> m;
+    mv.resize(n);
+    visited.resize(n, false);
+    
     for (int i = 0; i < n; i++) {
-        cin >> tem;
-        mv.push_back(tem);
-        visited.push_back(false);
-        mm[i] = tem;
+        cin >> mv[i];
     }
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     init();
     run(0);
-
     cout << maxRes;
     return 0;
 }
