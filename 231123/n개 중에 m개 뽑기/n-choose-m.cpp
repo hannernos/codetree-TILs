be508c03ba351818a575fd19721b2b3c27035764
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2023-11-23 19:12:33 +0900

    Title: n개 중에 m개 뽑기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231123/n개 중에 m개 뽑기/n-choose-m.cpp
+++ 231123/n개 중에 m개 뽑기/n-choose-m.cpp
@@ -3,53 +3,33 @@
 using namespace std;
 
 //var
-int N, M;// 1 ~ 10 
+int N, M; // 1 ~ 10 
 
-vector<int>mv;
-vector<bool>visited;
 vector<int> ivisited;
+
 //func
-
-
-void run(int a) { // a : idx, 1부터 시작
-    // 만들 수 있는 모든 조합을 구하시오 
+void run(int a, int start) { // a: depth, start: 시작 인덱스
     if (a == M) {
-        //출력하고 종료
-//        for (auto i : ivisited) {
-//            cout << i << " ";
-//        }cout << "\n";
-
         for (int i = 0; i < M; i++) {
-            cout << ivisited[M - 1 - i] << " ";
-        }cout << "\n";
+            cout << ivisited[i] << " ";
+        }
+        cout << "\n";
         return;
     }
 
-    for (auto i : mv) {
-        //visited
-        if (visited[i]) {
-            return;
-        }
+    for (int i = start; i <= N; i++) {
         ivisited.push_back(i);
-        visited[i] = true;
-        run(a + 1);
-        visited[i] = false;
+        run(a + 1, i + 1); // 다음 숫자는 현재 선택한 숫자 다음부터
         ivisited.pop_back();
     }
 }
 
 void init() {
     cin >> N >> M;
-    visited.push_back(false);
-    for (int i = 1; i <= N; i++) {
-        mv.push_back(i);
-        visited.push_back(false);
-    }
-
 }
+
 int main() {
-    // 여기에 코드를 작성해주세요.
     init();
-    run(0);
+    run(0, 1); // 초기 시작 인덱스는 1
     return 0;
 }
