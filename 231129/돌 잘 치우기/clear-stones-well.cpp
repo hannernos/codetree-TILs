4105d5405fb0e0c33ef45bbb91f7f697809e44dc
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2023-11-29 13:26:57 +0900

    Title: 돌 잘 치우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231129/돌 잘 치우기/clear-stones-well.cpp
@@ -0,0 +1,45 @@
+/*
+
+ 
+*/
+#include <iostream>
+
+using namespace std;
+
+
+//var
+int n,k;
+int m;
+
+//func
+
+
+void init(){
+    //var
+    int tem;
+    int r,c;
+    //logic
+    cin>>n>>k>>m;
+    for(int i=1;i<=n;i++){ // 
+        cin>>tem;
+
+    }
+    for(int i=0;i<k;i++){ // 시작점의 위치 r c
+        cin>>r>>c;
+
+    }
+
+    for(int i=0;i<m;i++){
+        cin>>m;
+
+    }
+
+}
+
+
+
+int main() {
+    init();
+
+    return 0;
+}
