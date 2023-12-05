a384747b7b67404e00571151b1afc0f707c1e0ba
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2023-12-05 22:14:12 +0900

    Title: 트리 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231205/트리 순회/the-tree-traversal.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+//var
+int n;
+
+struct Node{
+    char data;
+    Node* left;
+    Node* right;
+    Node(char data): data(data), left(nullptr), right(nullptr) {}
+};
+
+class Dtree{
+public:
+    Node* root;
+    Dtree(): root(nullptr) {}
+
+    void preorder(Node* node){
+        if(node != nullptr){
+            cout << node->data;
+            preorder(node->left);
+            preorder(node->right);
+        }
+    }
+
+    void inorder(Node* node){
+        if(node != nullptr){
+            inorder(node->left);
+            cout << node->data;
+            inorder(node->right);
+        }
+    }
+
+    void postorder(Node* node){
+        if(node != nullptr){
+            postorder(node->left);
+            postorder(node->right);
+            cout << node->data;
+        }
+    }
+};
+
+Dtree tree;
+unordered_map<char, Node*> nodes;
+
+void run(){
+    tree.preorder(tree.root);
+    cout << endl;
+    tree.inorder(tree.root);
+    cout << endl;
+    tree.postorder(tree.root);
+    cout << endl;
+}
+
+void init(){
+    cin >> n;
+    char a, b, c;
+
+    for(int i = 0; i < n; i++){
+        cin >> a >> b >> c;
+        if(nodes.find(a) == nodes.end()){
+            nodes[a] = new Node(a);
+            if(tree.root == nullptr) tree.root = nodes[a];
+        }
+
+        if(b != '.'){
+            nodes[a]->left = new Node(b);
+            nodes[b] = nodes[a]->left;
+        }
+
+        if(c != '.'){
+            nodes[a]->right = new Node(c);
+            nodes[c] = nodes[a]->right;
+        }
+    }
+}
+
+int main() {
+    init();
+    run();
+    return 0;
+}
