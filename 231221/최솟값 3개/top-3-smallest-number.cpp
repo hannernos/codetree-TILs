63cb05bbcf3f1da7a9b4fb62c003680c5bea8a63
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2023-12-21 01:31:55 +0900

    Title: 최솟값 3개, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231221/최솟값 3개/top-3-smallest-number.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+//func
+int run();
+void init();
+
+//var
+priority_queue<int> pq;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    init();
+    return 0;
+}
+
+int tem_mul;
+
+int run(int n,int tem){
+    int a,b,c;
+    if(n<3){
+        tem_mul = tem_mul*tem;
+
+        pq.push(tem);
+        return -1;
+    }else if(n==3){
+        tem_mul = tem_mul*tem;
+        pq.push(tem);
+        return tem_mul;
+    }
+
+    if(pq.top()>tem){
+        tem_mul = (tem_mul*tem)/(pq.top());
+
+        pq.pop();
+        pq.push(tem);
+    }
+    return tem_mul;
+}
+void init(){
+    int n;
+    int tem;
+    tem_mul = 1;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>tem;
+
+        cout<<run(i,tem)<<endl;
+
+    }
+}
