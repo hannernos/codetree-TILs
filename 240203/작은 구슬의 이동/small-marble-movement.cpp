adb20a9ccf5eb652ddd9f38ee9b9bef19a03e52b
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2024-02-03 18:04:06 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240203/작은 구슬의 이동/small-marble-movement.cpp
+++ 240203/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,48 +1,41 @@
-/*
-가다가 박음 방향전환 1초
-
-*/
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 //var 
-int dir_x[4]= {0,1,0,-1};
-int dir_y[4]= {1,0,-1,0};
+int dir_x[4]= {0, 1, 0, -1};
+int dir_y[4]= {1, 0, -1, 0};
 
-int r,c;
+int r, c;
 char d;
 int d_int;
 int n; // 2 50
 int t; // 1 100
 
-void run(){
-    int tx,ty;
-    tx = c;
-    ty = r;
-    int dx,dy;
+void run() {
+    int tx, ty;
+    tx = c - 1;
+    ty = r - 1;
+    int dx, dy;
     int td = d_int;
-    for(int i=0;i<t;i++){
+    for(int i = 0; i < t; i++) {
         dx = tx + dir_x[td];
         dy = ty + dir_y[td];
 
-        if (dy<1||dx<1||dy>n||dx>n){
-            td = (td+2)%4;
-        }else{
+        if(dy < 0 || dx < 0 || dy >= n || dx >= n) {
+            td = (td + 2) % 4;
+        } else {
             tx = dx;
             ty = dy;
         }
     }
-    cout<<ty<<" "<<td;
+    cout << ty + 1 << " " << tx + 1; 
 }
-
-void init(){
-    cin >> n>>t;
-    cin>>r>>c>>d;
-// int dir_x[4]= {0,1,0,-1};
-// int dir_y[4]= {1,0,-1,0};
-    switch (d){
+void init() {
+    cin >> n >> t;
+    cin >> r >> c >> d;
+    switch (d) {
         case 'U':
             d_int = 0;
             break;
@@ -59,7 +52,6 @@ void init(){
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     init();
     run();
     return 0;
