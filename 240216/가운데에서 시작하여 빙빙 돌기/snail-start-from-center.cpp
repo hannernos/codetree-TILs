ccaa92b8d1efcd1efb2b4bf5f77b037ac45418e9
Kim Dong Hyun <82031197+hannernos@users.noreply.github.com>
2024-02-16 22:41:24 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240216/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240216/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 void init();
 void run();
-bool check(int py,int px);
+bool check(int py, int px);
 void print();
 //var
 int n;
-int y,x;
-// R U L D , 아래가 위로
+int y, x;
 int dir;
-int dir_x[4] = {1, 0, -1, 0};
-int dir_y[4] = {0, -1, 0, 1};
+// R U L D , 아래가 위로
+// int dir_x[4] = { 1, 0, -1, 0 };
+// int dir_y[4] = { 0, -1, 0, 1 };
+// L D R U
+int dir_x[4] = {-1, 0, 1, 0 };
+int dir_y[4] = { 0,-1, 0, 1 };
 
 int arr[102][102];
 
@@ -25,41 +28,51 @@ int main() {
 }
 
 //func
-void init(){
-    cin>>n;
+void init() {
+    cin >> n;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            arr[i][j] = 0;
+        }
+    }
 }
-void run(){
-    int cnt = 1;
+void run() {
+    int cnt = n*n;
     dir = 0;
 
-    y = (n / 2) + 1;
-    x = (n / 2) + 1;
+    // y = (n / 2) + 1;
+    // x = (n / 2) + 1;
+    y = n;
+    x = n;
 
-    for(int i=0;i<(n*n);i++){
+    for (int i = 0; i < (n * n); i++) {
 
         arr[y][x] = cnt;
 
-        if(!check(y + dir_y[dir],x + dir_x[dir])){
-            dir = (dir + 1) % 4;    
-        }else{}
+        if (!check(y + dir_y[dir], x + dir_x[dir])) {
+            dir = (dir + 1) % 4;
+        }
+        else {}
 
         y = y + dir_y[dir];
         x = x + dir_x[dir];
-        cnt++;
+        cnt--;
+
+        //print();
     }
 }
 
-bool check(int py, int px){
-    if(py<1||px<1||py>n||px>n||arr[py][px]>0){
+bool check(int py, int px) {
+    if (py<1 || px<1 || py>n || px>n || arr[py][px] > 0) {
         return false;
     }
     return true;
 }
 
-void print(){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<arr[i][j]<< " " ;
-        }cout<<"\n";
-    }cout<<"\n";
+void print() {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << arr[i][j] << " ";
+        }cout << "\n";
+    }cout << "\n";
 }
